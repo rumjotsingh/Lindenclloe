bed346adbd1b29abea0d0651a57fec1ceb20acab
rumjotsingh <155811126+rumjotsingh@users.noreply.github.com>
2024-11-23 08:03:12 +0530

    Update index.jsx

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,5 +1,5 @@
 import axios  from "axios";
-export const BASE_URL ="http://localhost:9090"
+export const BASE_URL ="https://lindenclone.onrender.com"
 export const clientServer=axios.create({
     baseURL:BASE_URL
 });
